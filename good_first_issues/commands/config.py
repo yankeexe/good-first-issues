4f2d6a3085aa7b3eec189bb29ec6d854b74dc1f4
Yankee <yankee.exe@gmail.com>
2024-10-31 18:00:24 +0400

    Update help text for config sub-command

--- good_first_issues/commands/config.py
+++ good_first_issues/commands/config.py
@@ -6,7 +6,11 @@ from good_first_issues import utils
 @click.command()
 def config():
     """
-    Prompt user to enter Github Personal Access Token.
+    Prompt user to enter Github Fine-grained Personal Access Token.
+
+    Generate token here:
+
+        https://github.com/settings/tokens?type=beta
     """
     token: str = click.prompt(
         "Enter your GitHub Access Token (hidden)", hide_input=True
